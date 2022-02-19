4d27b879e935621aaefada29331ad72f7a157560
hubertwojtas <hubert.wojtas32@gmail.com>
2022-02-19 12:05:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const path = require("path");
+const express = require("express");
+
+const functions = require("./functions")
+const app = express();
+const port = 3000;
+
+app.set("view engine","hbs");
+app.use("/assets", express.static(path.join(__dirname, "./assets")));
+app.use("/js", express.static(path.join(__dirname, "./js")));
+
+app.get("/", function (req, res) {
+    res.render("index", {
+        pageTitle: "Lekcja ALK 19.02.2022",
+        x: 4,
+        y: functions.add(1,2),
+    });
+})
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+app.listen(port, () => {
+    console.log(`Serwer działa na porcie ${port}`);
+});
+
---
+++ functions.js
@@ -0,0 +1,11 @@
+const sayHello = () => console.log("Hello2");
+sayHello();
+
+const add = (a,b) => {
+return a+b;
+}
+
+module.exports = {
+    sayHello,
+    add
+}
